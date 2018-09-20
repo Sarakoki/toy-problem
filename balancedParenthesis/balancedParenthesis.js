902ea6f6fc1f5bb8dbdae520b8c9fbc337ae7214
sarakoki <sarakoki94@gmail.com>
2018-09-20 17:00:54 +0300

    solved

--- balancedParenthesis/balancedParenthesis.js
+++ balancedParenthesis/balancedParenthesis.js
@@ -18,8 +18,25 @@ string:
 ----------------------------------
 */
 
-function isBalanced (string) {
-	// write your code here
+function isBalanced(string) {
+  // write your code here
+  var arr = [];
+  var arr1 = [];
+  if (string === "") {
+    return true;
+  } else var string1 = string.split("");
+  for (var i = 0; i < string1.length; i++) {
+    if (string[0] !== "(") {
+      return false;
+    } else if (string1[i] === "(") {
+      arr.push(string1[i]);
+    } else if (string1[i] === ")") {
+      arr1.push(string1[i]);
+    }
+  }
+  if (arr.length === arr1.length) {
+    return true;
+  } else {
+    return false;
+  }
 }
-
-
